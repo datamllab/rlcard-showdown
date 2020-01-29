e663cac01c82b7d9a2223c374c8a169273172c7f
songyih <songyih1996@gmail.com>
2020-01-28 21:29:53 -0800

    try game speed controller

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import DoudizhuGameView from './view/DoudizhuGameView';
+import LeducHoldemGameView from './view/LeducHoldemGameView';
 
 function App() {
     return (
-        <div>
-            <DoudizhuGameView />
-        </div>
+        <Router>
+            <Route path="/doudizhu" component={DoudizhuGameView} />
+            <Route path="/leduc-holdem"  component={LeducHoldemGameView} />
+        </Router>
     );
 }
 

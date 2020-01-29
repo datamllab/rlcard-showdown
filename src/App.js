62f71d36559baea6894dfe7b7d504ff7d2b8e193
songyih <songyih1996@gmail.com>
2020-01-28 21:38:37 -0800

    redirect root dir to leduc holdem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import DoudizhuGameView from './view/DoudizhuGameView';
 import LeducHoldemGameView from './view/LeducHoldemGameView';
 
 function App() {
     return (
         <Router>
+            <Route exact path="/">
+                <Redirect to="/leduc-holdem" />
+            </Route>
             <Route path="/doudizhu" component={DoudizhuGameView} />
             <Route path="/leduc-holdem"  component={LeducHoldemGameView} />
         </Router>

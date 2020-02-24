bf3546922dddae69d152c70d2bc813caa34fffcc
lpan <sophie.leipan@gmail.com>
2020-02-24 10:47:33 -0800

    changed style for leduc

--- src/components/GameBoard/LeducHoldemGameBoard.js
+++ src/components/GameBoard/LeducHoldemGameBoard.js
@@ -2,17 +2,65 @@ import React from 'react';
 import { translateCardData, millisecond2Second } from '../../utils'
 
 import '../../assets/leducholdem.scss';
+import Player1 from '../../assets/images/Portrait/Player1.png'
+import Player2 from '../../assets/images/Portrait/Player2.png'
+import PlaceHolderPlayer from '../../assets/images/Portrait/Player.png';
+
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 
 class LeducHoldemGameBoard extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    computePlayerPortrait(playerId, playerIdx){
+        if(this.props.playerInfo.length > 0){
+            return this.props.playerInfo[playerIdx].id === 0 ?
+                <div>
+                    <img src={Player1} alt={"Player 1"} height="70%" width="70%" />
+                    <Chip
+                        avatar={<Avatar>ID</Avatar>}
+                        label={playerId}
+                        clickable
+                        color="primary"
+                    />
+                </div>
+                :
+                <div>
+                    <img src={Player2} alt={"Player 2"} height="70%" width="70%" />
+                    <Chip
+                        avatar={<Avatar>ID</Avatar>}
+                        label={playerId}
+                        clickable
+                        color="primary"
+                    />
+                </div>
+        }else
+            return (
+                <div>
+                    <img src={PlaceHolderPlayer} alt={"Player"} height="70%" width="70%" />
+                    <Chip
+                        avatar={<Avatar>ID</Avatar>}
+                        label={playerId}
+                        clickable
+                        color="primary"
+                    />
+                </div>
+            )
+    }
+
+    computeActionImage(action) {
+        if (action.length > 0) {
+            return <img src={require('../../assets/images/Actions/' + action + '.png')} alt={action} height="80%" width="100%" />
+        }
+    }
+
     computeHand(card) {
         const [rankClass, suitClass, rankText, suitText] = translateCardData(card);
         return (
-            <div className="playingCards faceImages">
-                <div className={`card ${rankClass} ${suitClass}`}>
+            <div className="playingCards faceImages unselectable">
+                <div className={`card ${rankClass} full-content ${suitClass}`}>
                     <span className="rank">{rankText}</span>
                     <span className="suit">{suitText}</span>
                 </div>
@@ -22,9 +70,13 @@ class LeducHoldemGameBoard extends React.Component {
 
     playerDecisionArea(playerIdx){
         if(this.props.currentPlayer === playerIdx){
-            return <div className="non-card"><span>{`Consideration Time: ${millisecond2Second(this.props.considerationTime)}s`}</span></div>
+            return (
+                <div className={"timer"}>
+                    <div className="timer-text">{millisecond2Second(this.props.considerationTime)}</div>
+                </div>
+            )
         }else{
-            return <div className="non-card"><span>{this.props.latestAction[playerIdx]}</span></div>
+            return <div className="non-card">{this.computeActionImage(this.props.latestAction[playerIdx])}</div>
         }
     }
 
@@ -39,7 +91,7 @@ class LeducHoldemGameBoard extends React.Component {
             const [rankClass, suitClass, rankText, suitText] = translateCardData(this.props.publicCard);
             return (
                 <div className="playingCards faceImages">
-                    <div className={`card ${rankClass} ${suitClass}`}>
+                    <div className={`card ${rankClass} full-content ${suitClass}`}>
                         <span className="rank">{rankText}</span>
                         <span className="suit">{suitText}</span>
                     </div>
@@ -65,14 +117,15 @@ class LeducHoldemGameBoard extends React.Component {
                 topId = found.id;
         }
         return (
-            <div className="leduc-holdem-wrapper" style={{width: "100%", height: "100%", backgroundColor: "#ffcc99", position: "relative"}}>
+            <div className="leduc-holdem-wrapper">
                 <div id={"bottom-player"}>
                     <div className="played-card-area">
                         {bottomIdx >= 0 ? this.playerDecisionArea(bottomIdx) : ""}
                     </div>
                     <div className="player-main-area">
                         <div className="player-info">
-                            <span>{`Player Id: ${bottomId}\nBet: ${this.props.pot[bottomIdx]}`}</span>
+                            {this.computePlayerPortrait(bottomId, bottomIdx)}
+                            <span>{`Bet: ${this.props.pot[bottomIdx]}`}</span>
                         </div>
                         {bottomIdx >= 0 ? <div className="player-hand">{this.computeHand(this.props.hands[bottomIdx])}</div> : <div className="player-hand-placeholder"><span>Waiting...</span></div>}
                     </div>
@@ -80,7 +133,8 @@ class LeducHoldemGameBoard extends React.Component {
                 <div id={"top-player"}>
                     <div className="player-main-area">
                         <div className="player-info">
-                            <span>{`Player Id: ${topId}\nBet: ${this.props.pot[topIdx]}`}</span>
+                            {this.computePlayerPortrait(topId, topIdx)}
+                            <span>{`Bet: ${this.props.pot[topIdx]}`}</span>
                         </div>
                         {topIdx >= 0 ? <div className="player-hand">{this.computeHand(this.props.hands[topIdx])}</div> : <div className="player-hand-placeholder"><span>Waiting...</span></div>}
                     </div>
